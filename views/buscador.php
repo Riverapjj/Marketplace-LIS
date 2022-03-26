923ecf1b13d836daa9eab973a7930f5059637407
Riverapjj <keko.rai.jr@gmail.com>
2022-03-25 18:19:45 -0600

    Categoria hogar diseño

---
+++ views/buscador.php
@@ -0,0 +1,22 @@
+<div class="p-2" style="background-color: rgb(90, 90, 209);">
+    <div class="input-group">
+        <i class="bi bi-search input-group-text" id="basic-addon1"></i>
+        <input type="text" class="form-control" list="datalistOptions" placeholder="Busca aquí..."
+            aria-label="Busca aquí..." aria-describedby="basic-addon1">
+        <datalist id="datalistOptions">
+            <option value="San Francisco">
+            <option value="New York">
+            <option value="Seattle">
+            <option value="Los Angeles">
+            <option value="Chicago">
+        </datalist>
+
+        <button class="btn btn-dark ms-2 text-white">
+            <i class="bi bi-cart4 me-1"></i>Carrito
+            <span class="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-warning text-black">
+                99+
+                <span class="visually-hidden">unread messages</span>
+            </span>
+        </button>
+    </div>
+</div>
